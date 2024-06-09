e060753623fed5873faafd2ee73643cc5b20cbd2
Himanshu <kyadav.9643@gmail.com>
2024-06-09 22:26:44 +0530

    Add search, editor, signin, and signup functionality to navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar.component';
+import UserAuthForm from './pages/userAuthForm.page';
 
 const App = () => {   
     return (
-
-        <Navbar />
-    )
+        <Routes>    
+            <Route path="/" element={<Navbar />} >
+                <Route path="/signin" element={<UserAuthForm type={"sign-in"} />} />
+                <Route path="/signup" element={<UserAuthForm type={"sign-up"} />} />
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
