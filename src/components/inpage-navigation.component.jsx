bf9ed70883f649240c1d0e1e53c7a1528d9b0bfa
Himanshu <kyadav.9643@gmail.com>
2024-07-23 15:12:36 +0530

    refactor: Update InPageNavigation component to improve code readability and maintainability

--- src/components/inpage-navigation.component.jsx
+++ src/components/inpage-navigation.component.jsx
@@ -1,41 +1,57 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from "react";
 
-const InPageNavigation = ({routes, defaultActiveIndex}) => {
-    let activeTabLineRef = useRef();
-    let [inPageNavIndex, setInPageNavIndex] = useState(0);
+const InPageNavigation = ({
+  routes,
+  defaultHidden = [],
+  defaultActiveIndex = 0,
+  children,
+}) => {
+  let activeTabLineRef = useRef();
+  let activeTabRef = useRef();
 
-    const changePageState = (btn, i) => {
-        let {offsetWidth, offsetLeft} = btn;
-        activeTabLineRef.current.style.width = offsetWidth + "px";
-        activeTabLineRef.current.style.left = offsetLeft + "px";
+  let [inPageNavIndex, setInPageNavIndex] = useState(defaultActiveIndex);
 
-        setInPageNavIndex(i);
-    };
+  const changePageState = (btn, i) => {
+    let { offsetWidth, offsetLeft } = btn;
+    activeTabLineRef.current.style.width = offsetWidth + "px";
+    activeTabLineRef.current.style.left = offsetLeft + "px";
+
+    setInPageNavIndex(i);
+  };
+
+  useEffect(() => {
+    changePageState(activeTabRef.current, defaultActiveIndex);
+  }, []);
 
   return (
     <>
       <div className="relative mb-8 bg-white border-b border-grey flex flex-nowrap overflow-auto">
-        {
-            routes.map((route, i) => {
-            return (
-                <button
-                key={i}
-                className={
-                    "p-4 px-5 capitalize " +
-                    (inPageNavIndex == i ? "text-black font-medium" : "text-dark-grey")
-                }
-                onClick={(e) => {
-                    changePageState(e.target, i);
-                }}
-                >
-                {route}
-                </button>
-            );
+        {routes.map((route, i) => {
+          return (
+            <button
+              ref={i == defaultActiveIndex ? activeTabRef : null}
+              key={i}
+              className={
+                "p-4 px-5 capitalize " +
+                (inPageNavIndex == i
+                  ? "text-black font-medium "
+                  : "text-dark-grey ") +
+                (defaultHidden.includes(route) ? "md:hidden " : " ")
+              }
+              onClick={(e) => {
+                changePageState(e.target, i);
+              }}
+            >
+              {route}
+            </button>
+          );
         })}
         <hr ref={activeTabLineRef} className="absolute bottom-0 duration-300" />
       </div>
+
+      {Array.isArray(children) ? children[inPageNavIndex] : children}
     </>
   );
-}
+};
 
-export default InPageNavigation
+export default InPageNavigation;
--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -1,28 +1,54 @@
-import React from 'react'
-import AnimationWrapper from '../common/page-animation'
-import InPageNavigation from '../components/inpage-navigation.component'
+import React, { useEffect, useState } from "react";
+import AnimationWrapper from "../common/page-animation";
+import InPageNavigation from "../components/inpage-navigation.component";
+import axios from "axios";
+import Loader from "../components/loader.component";
 
 const HomePage = () => {
+  let [blogs, setBlog] = useState(null);
+
+  const fetchLatestBlogs = () => {
+    axios
+      .get(`${import.meta.env.VITE_SERVER_URL}/blog/latest-blogs`)
+      .then(({ data }) => {
+        setBlog(data.blogs);
+        console.log(data.blogs);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    fetchLatestBlogs();
+  }, []);
+
   return (
     <AnimationWrapper>
-        <section className='h-cover flex justi gap-10 '>
-            
-            {/* Latest blogs */}
-            <div className='w-full'>
+      <section className="h-cover flex justi gap-10 ">
+        {/* Latest blogs */}
+        <div className="w-full">
+          <InPageNavigation
+            routes={["home", "trending blogs"]}
+            defaultHidden={["trending blogs"]}
+          >
+            <>
+              {blogs == null ? (
+                <Loader />
+              ) : (
+                blogs.map((blog, i) => {
+                  return <h1 key={i}>{blog.title}</h1>;
+                })
+              )}
+            </>
 
-                <InPageNavigation routes={["home", "trending blogs"]}>
+            <h1>Trending blogs</h1>
+          </InPageNavigation>
+        </div>
 
-                </InPageNavigation>
-            
-            </div>
-            
-            {/* filters   */}
-            <div>
-
-            </div>
-        </section>
+        {/* filters   */}
+        <div></div>
+      </section>
     </AnimationWrapper>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
