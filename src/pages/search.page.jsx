408bf9850667d5df8a223398870c4275c42eaf2e
Himanshu <kyadav.9643@gmail.com>
2024-07-26 00:52:53 +0530

    feat: Add UserCard component and fetch users based on search query

--- src/pages/search.page.jsx
+++ src/pages/search.page.jsx
@@ -8,11 +8,13 @@ import NoDataMessage from '../components/nodata.component';
 import LoadMoreDataBtn from '../components/load-more.component';
 import axios from 'axios';
 import { filterPaginationData } from '../common/filter-pagination-data';
+import UserCard from '../components/usercard.component';
 
 const SearchPage = () => {
 
   let {query} = useParams();
   let [blogs, setBlog] = useState(null);
+  let [users, setUsers] = useState(null);
 
   const searchBlog = ({page = 1, create_new_arr = false}) => {
     axios
@@ -33,13 +35,50 @@ const SearchPage = () => {
       .catch((err) => console.log(err));
   }
 
+  const fetchUsers = () => {
+    axios
+      .post(`${import.meta.env.VITE_SERVER_URL}/users/search-users`, {query})
+      .then(({ data: {users} }) => {
+        setUsers(users);
+      })
+      .catch((err) => console.log(err));
+  
+  }
+
   useEffect(() => {
     resetState();
     searchBlog({page: 1, create_new_arr: true})
+    fetchUsers();
   }, [query])
 
   const resetState = () => {
     setBlog(null);
+    setUsers(null);
+  }
+
+  const UserCardWrapper = () => {
+
+    return (
+      <>
+        {users == null ? (
+          <Loader />
+        ) : users.length ? (
+          users.map((user, i) => {
+            return (
+              <AnimationWrapper
+                key={i}
+                transition={{ duration: 1, delay: i * 0.08 }}
+              >
+                <UserCard user={user}/>
+              </AnimationWrapper>
+            );
+          })
+        ) : (
+          <NoDataMessage message="No users found" />
+        )}
+      </>
+    );
+
   }
 
   return (
@@ -71,7 +110,17 @@ const SearchPage = () => {
             )}
             <LoadMoreDataBtn state={blogs} fetchDataFucn={searchBlog}/>
           </>
+
+          < UserCardWrapper />
         </InPageNavigation>
+      </div>
+
+      <div className='min-w-[40%] lg:min-w-[350px] max-w-min border-l border-grey pl-8 pt-3 max-md:hidden'>
+            <h1 className='font-medium text-xl mb-8'>
+                User related to search <i className='fi fi-rr-user mt-1'></i>
+            </h1>
+
+            < UserCardWrapper />
       </div>
     </section>
   );
