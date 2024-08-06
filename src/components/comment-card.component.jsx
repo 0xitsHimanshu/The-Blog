5ee2c0da8514c0cbfae2635f9d4bc420eb224a34
Himanshu <kyadav.9643@gmail.com>
2024-08-06 23:52:10 +0530

    feat: Add reply functionality to CommentCard component

--- src/components/comment-card.component.jsx
+++ src/components/comment-card.component.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import { getDay } from '../common/date';
+import { UserContext } from '../App';
+import CommentField from './comment-field.component';
 
 
 const CommentCard = ({index, leftVal, commentData}) => {
   
-    let {commented_by :{ personal_info: {profile_img, fullname, username}}, commentedAt, comment} = commentData;
+    let {commented_by :{ personal_info: {profile_img, fullname, username}}, commentedAt, comment, _id } = commentData;
+    // let {userAuth: {accessToken, user: { username, fullname, profile_img}}} = useContext(UserContext)
+    const { userAuth } = useContext(UserContext);
+    const accessToken = userAuth?.accessToken;
+    const [ isReplying, setIsReplying ] = useState(false);
+    
     // console.log(commentData);
+    const handleReplyClick = () => {
+        if(!accessToken){
+            return toast.error('You need to login to reply')
+        }
+
+        setIsReplying(preValue => !preValue);
+    }
+
     return (
     <div className='w-full' style={{paddingLeft:`${leftVal * 10}px` }}>
         <div className='my-5 p-6 rounded-md border border-grey'>
@@ -17,6 +32,18 @@ const CommentCard = ({index, leftVal, commentData}) => {
             </div>
 
             <p className='text-xl ml-3'>{comment}</p>
+
+            <div className='flex gap-5 items-center mt-5'>
+                <button className='underline mb-5' onClick={handleReplyClick}>
+                    reply
+                </button>
+            </div>
+
+            {
+               isReplying ? 
+                <CommentField action="reply" index={index} replyingTo={_id} setReplying={setIsReplying}/>
+               : "" 
+            }
         </div>
     </div>
   )
--- src/components/comment-field.component.jsx
+++ src/components/comment-field.component.jsx
@@ -1,48 +1,93 @@
-import React, { useContext, useState } from 'react'
-import { UserContext } from '../App';
-import toast, { Toaster } from 'react-hot-toast';
-import { BlogContext } from '../pages/blog.page';
-import axios from 'axios';
+import React, { useContext, useState } from "react";
+import { UserContext } from "../App";
+import toast, { Toaster } from "react-hot-toast";
+import { BlogContext } from "../pages/blog.page";
+import axios from "axios";
 
-const CommentField = ({action}) => {
-    let {blog ,blog: {_id, author: {_id: blog_author}, comments, comments: {results: commentsArr }, activity, activity: {total_comments, total_parent_comments} },setBlog, totalParentCommentsLoaded, setTotalParentCommentsLoaded} = useContext(BlogContext)
-    // let {userAuth: {accessToken, user: { username, fullname, profile_img}}} = useContext(UserContext)
-    const { userAuth } = useContext(UserContext);
+const CommentField = ({ action, index=undefined, replyingTo=undefined, setReplying }) => {
+  let {
+    blog,
+    blog: {
+      _id,
+      author: { _id: blog_author },
+      comments,
+      comments: { results: commentsArr },
+      activity,
+      activity: { total_comments, total_parent_comments },
+    },
+    setBlog,
+    totalParentCommentsLoaded,
+    setTotalParentCommentsLoaded,
+  } = useContext(BlogContext);
+  // let {userAuth: {accessToken, user: { username, fullname, profile_img}}} = useContext(UserContext)
+  const { userAuth } = useContext(UserContext);
 
-    // Ensure userAuth and user are defined before destructuring
-    const accessToken = userAuth?.accessToken;
-    const username = userAuth?.user?.username ?? '';
-    const fullname = userAuth?.user?.fullname ?? '';
-    const profile_img = userAuth?.user?.profile_img ?? '';
-    
-    const [comment, setComment] = useState("");
-    const handleComment =  () => {
-        if(!accessToken){
-            return toast.error('You need to login to comment')
-        }
+  // Ensure userAuth and user are defined before destructuring
+  const accessToken = userAuth?.accessToken;
+  const username = userAuth?.user?.username ?? "";
+  const fullname = userAuth?.user?.fullname ?? "";
+  const profile_img = userAuth?.user?.profile_img ?? "";
 
-        if(!comment.length){
-            return toast.error('Write something to comment...')
-        }
+  const [comment, setComment] = useState("");
 
-        axios
-         .post(`${import.meta.env.VITE_SERVER_URL}/blog/add-comment/`, {_id, comment, blog_author}, {headers: {Authorization: `Bearer ${accessToken}`}})
-         .then(({data}) => {
-            setComment("");
-            data.commented_by = { personal_info: {username, fullname, profile_img}}
-            let newCommentArr;
+  const handleComment = () => {
+    if (!accessToken) {
+      return toast.error("You need to login to comment");
+    }
+
+    if (!comment.length) {
+      return toast.error("Write something to comment...");
+    }
+
+    axios
+      .post(
+        `${import.meta.env.VITE_SERVER_URL}/blog/add-comment/`,
+        { _id, comment, blog_author, replying_to: replyingTo },
+        { headers: { Authorization: `Bearer ${accessToken}` } }
+      )
+      .then(({ data }) => {
+
+        setComment("");
+        data.commented_by = {
+          personal_info: { username, fullname, profile_img },
+        };
+        let newCommentArr;
+
+        if(replyingTo){
+            commentsArr[index].children.push(data._id);
+            data.childrenLevel = commentsArr[index].childrenLevel + 1;
+
+            commentsArr[index].isReplyLoaded = true;
+            commentsArr.splice(index + 1, 0, data);
+            newCommentArr = commentsArr
+
+            setReplying(false)
+
+        } else {
             data.childrenLevel = 0;
             newCommentArr = [data, ...commentsArr];
-            let parentCommentIncVal = 1;
+        }
 
-            setBlog({...blog, comments: {...comments, results: newCommentArr}, activity: {...activity, total_comments: total_comments + 1, total_parent_comments: total_parent_comments + parentCommentIncVal}})
-            
-            setTotalParentCommentsLoaded(preValue => preValue + parentCommentIncVal);
-         })
-         .catch(err => {
-             console.log(err.message);
-         })
-    };
+        let parentCommentIncVal = replyingTo ? 0 : 1;
+
+        setBlog({
+          ...blog,
+          comments: { ...comments, results: newCommentArr },
+          activity: {
+            ...activity,
+            total_comments: total_comments + 1,
+            total_parent_comments: total_parent_comments + parentCommentIncVal,
+          },
+        });
+
+        setTotalParentCommentsLoaded(
+          (preValue) => preValue + parentCommentIncVal
+        );
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
 
   return (
     <>
@@ -53,12 +98,11 @@ const CommentField = ({action}) => {
         placeholder="Leave a comment..."
         className="input-box pl-5 placeholder:text-dark-grey resize-none h-[150px] overflow-auto"
       ></textarea>
-      <button 
-        onClick={handleComment}
-        className="btn-dark mt-5 px-10"
-        >{action}</button>
+      <button onClick={handleComment} className="btn-dark mt-5 px-10">
+        {action}
+      </button>
     </>
   );
-}
+};
 
-export default CommentField
+export default CommentField;
