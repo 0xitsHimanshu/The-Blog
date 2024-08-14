9a6c266b3eb52b1dbb22c1914d29e5cbd4fdc496
Himanshu <kyadav.9643@gmail.com>
2024-08-14 23:51:23 +0530

    fix: Fix conditional comparison operator in SideNav component

--- src/components/sidenavbar.component.jsx
+++ src/components/sidenavbar.component.jsx
@@ -36,7 +36,7 @@ const SideNav = () => {
 
     return (
         
-        accessToken == null ? <Navigate to={"/signin"} /> :  
+        accessToken === null ? <Navigate to={"/signin"} /> :  
 
         <>
             <section className='relative flex gap-10 py-0 m-0 max-md:flex-col'>
@@ -55,7 +55,7 @@ const SideNav = () => {
                     </div>
                     
                     {/* parent div below  */}
-                    <div className={'min-w-[200px] h-[calc(100vh-80px-60px)] md:h-cover md:sticky top-24 overflow-y auto p-6 md:pr-0 md: border-grey md: border-r absolute max-md:top-[64px] bg-white max-md:w-[calc(100%+80px)] max-md: px-16 max-md:-ml-7 duration-500 '+ (!showSideNav ? "max-md:opacity-0 pointer-events-none ": "max-md:opacity-100 pointer-events-auto")}>
+                    <div className={'min-w-[200px] h-[calc(100vh-80px-60px)] h-cover md:sticky top-24 overflow-y auto p-6 md:pr-0 md: border-grey md: border-r absolute max-md:top-[64px] bg-white max-md:w-[calc(100%+80px)] max-md: px-16 max-md:-ml-7 duration-500 '+ (!showSideNav ? "max-md:opacity-0 pointer-events-none ": "max-md:opacity-100 pointer-events-auto")}>
                         
                         <h1 className='text-xl text-dark-grey mb-3'>Dashboard</h1>
                         <hr className='border-grey -ml-6 mb-8 mr-6' />
